80dbd2d0bbca746d7cbd0d94c3b5ab6f9de2f033
S2301 <belludianupsiri23@gmail.com>
2022-09-08 15:20:44 +0530

    first commit

---
+++ include/footer.php
@@ -0,0 +1,72 @@
+<footer class="full-row bg-secondary p-0">
+            <div class="container">
+                <div  class="row">
+                    <div class="col-lg-12">
+                        <div class="divider py-40">
+                            <div class="row">
+                                <div class="col-md-12 col-lg-4">
+                                    <div class="footer-widget mb-4">
+                                        <div class="footer-logo mb-4"> <a href="#"><img class="logo-bottom" src="images/logo/logo-white.png" alt="image"></a> </div>
+                                        <p class="pb-20 text-white">Launched in 2021, Homex aims to be India's No.1 online Property marketplace to buy, sell, and rent residential and commercial properties.</p>
+										<p class="pb-20 text-white"></p>
+                                        </div>
+                                </div>
+                                <div class="col-md-12 col-lg-8">
+                                    <div class="row">
+                                        <div class="col-md-4 col-lg-4">
+                                            <div class="footer-widget footer-nav mb-4">
+                                                <h4 class="widget-title text-white double-down-line-left position-relative">Support</h4>
+                                                <ul class="hover-text-primary">
+                                                    <!--<li><a href="#" class="text-white">Forum</a></li>-->
+                                                    <!--<li><a href="#" class="text-white">Statistics</a></li>-->
+                                                    <li><a href="#" class="text-white">Terms and Condition</a></li>
+                                                    <li><a href="#" class="text-white">Get Support</a></li>
+                                                    <li><a href="#" class="text-white">Freequenly Ask Question</a></li>
+                                                    <!--<li><a href="" class="text-white">Contact</a></li>-->
+                                                    <li><a href="/Real-Estate-Php-main/contact.php" class="text-white">Contact</a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4 col-lg-4">
+                                            <div class="footer-widget footer-nav mb-4">
+                                                <h4 class="widget-title text-white double-down-line-left position-relative">Quick Links</h4>
+                                                <ul class="hover-text-primary">
+                                                    <li><a href="#" class="text-white">About Us</a></li>
+                                                    <li><a href="#" class="text-white">Featured Property</a></li>
+                                                    <li><a href="#" class="text-white">Become A Member</a></li>
+                                                    <li><a href="#" class="text-white">Submit Property</a></li>
+                                                    <li><a href="#" class="text-white">How It Work</a></li>
+                                                    <li><a href="#" class="text-white">Our Agents</a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4 col-lg-4">
+                                            <div class="footer-widget">
+                                                <h4 class="widget-title text-white double-down-line-left position-relative">Contact Us</h4>
+                                                <ul class="text-white">
+                                                    <li class="hover-text-primary"><i class="fas fa-map-marker-alt text-white mr-2 font-13 mt-1"></i>#345,JH Plaza,MG Road,Bengaluru</li>
+                                                    <li class="hover-text-primary"> <i class="fas fa-phone-alt text-white mr-2 font-13 mt-1"></i>1234567898</li>
+													<li class="hover-text-primary"> <i class="fas fa-phone-alt text-white mr-2 font-13 mt-1"></i>6677788333</li>
+                                                    <li class="hover-text-primary"><i class="fas fa-envelope text-white mr-2 font-13 mt-1"></i>communications@homex.com</li>
+                                                </ul>
+                                            </div>
+                                            <div class="footer-widget media-widget mt-4 text-white hover-text-primary"> <a href="#"><i class="fab fa-facebook-f"></i></a> <a href="#"><i class="fab fa-twitter"></i></a> <a href="#"><i class="fab fa-google-plus-g"></i></a> <a href="#"><i class="fab fa-linkedin-in"></i></a> <a href="#"><i class="fas fa-rss"></i></a> </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row copyright">
+                    <div class="col-sm-6"> <span class="text-white">© 2021 Homex All right reserved</span> </div>
+                    <div class="col-sm-6">
+                        <ul class="line-menu text-white hover-text-primary float-right">
+                            <li><a href="#">Privacy & Policy</a></li>
+                            <li>|</li>
+                            <li><a href="#"> Site Map</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
