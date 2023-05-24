4b6256cfee89b7186ec67d6779eb0fd03b0fba5e
S2301 <84118335+S2301@users.noreply.github.com>
2023-05-24 23:22:28 +0530

    Update index.php

--- index.php
+++ index.php
@@ -96,7 +96,7 @@ include("config.php");
                                     </div>
                                     <div class="col-md-4 col-lg-2">
                                         <div class="form-group">
-                                            <button type="submit" name="filter" class="btn btn-primary w-100">Search Property</button>
+                                            <button type="submit" name="filter" class="btn btn-primary w-100">Search </button>
                                         </div>
                                     </div>
                                 </div>
